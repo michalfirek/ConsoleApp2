4d4be03bd7f3677362573623b52d5919a797206d
michalfirek <michalfirek@icloud.com>
2025-01-22 09:47:47 +0100

    GetMiddle Pass

--- Program.cs
+++ Program.cs
@@ -641,6 +641,11 @@ namespace ConsoleApp2
 			}
 			return false;
 		}
+
+		public static string GetMiddle(string s)
+		{
+			return s.Length % 2 == 0 ? s[(s.Length / 2)-1].ToString() + s[(s.Length / 2)].ToString() : s[(s.Length)/2].ToString();
+		}
 	};
 }
 
